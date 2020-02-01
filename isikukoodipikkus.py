3fe8b81833037fa58a3e0ec8058ef02794204d86
Georg Kiviväli <54536253+lollipas@users.noreply.github.com>
2020-02-01 13:15:15 +0200

    Add files via upload

---
+++ isikukoodipikkus.py
@@ -0,0 +1,26 @@
+def isikukood():
+
+    ik=open("IK.txt", "r")
+    
+
+    Lisikukood = 11
+    iktulp=[]
+    nimtulp=[]
+    
+    for line in ik:
+        h = line.split("\t")
+        iktulp.append(h[0])
+        nimtulp.append(h[1])
+
+    print(iktulp)
+    print(nimtulp)
+
+    #i = input("sisestage oma isikukood => ")
+    #Linput= len(i)
+    
+    #if(Lisikukood > Linput):
+      #  print("Vigane kood - pikkus on vale. Päringu lõpp")
+    
+ 
+
+isikukood()
